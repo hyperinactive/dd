0b3b080ebaaaa80b28b2bfe6972f8915f83301ca
hyperinactive <hyperinactive.box@gmail.com>
2021-06-05 21:31:36 +0200

    basics_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,87 @@
-// built-in widgets, themes, material, base classes
 import 'package:flutter/material.dart';
 
-// to run our app we need a 'runApp() method' from the material dependency
 void main() {
-  // anonymous instance of our MyApp() class
   runApp(MyApp());
 }
 
-// javascript arrow function
-// shorthand for funcitons with just one expression
-// void main() => runApp(MyApp());
+// types of widgets:
+// Output/Input
+// Layout and Control
+// Container
 
-// StatelessWidget - base class - empty widget
-// every widget must extend a Stateless or Stateful
-class MyApp extends StatelessWidget {
-  // necessary method for classes, reqires BuildContext
-  // this method is of class Widget, since it returns, well.. a Widget
-  // build is called every time flutter needs to render smth onto the screen
-  // build is always responsible for widget rendering - every widget has its own build method
+// doesn't expect to have a state of any kind, doesn't rerender based on internal state
+// class MyApp extends StatelessWidget {
+
+// Stateful widget consists of 2 classes
+// createState implementation should return the linked state of the widget
+// build and all of the state data stays in the state class
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // _ means private, as in, it can be "seen" within the file (kind of like protected)
+    // public -> _private class
+    // same goes for variables
+    return _MyAppState();
+  }
+}
+
+// secondary class of the stateful app which contains the internal state
+// extends State
+// State is a generic class and we need to point to our Stateful class to attach it
+// <pointerToTheClass>
+class _MyAppState extends State<MyApp> {
+  int _questionIndex = 0;
+
+  void _answer() {
+    // the UI elements won't rerender unless we change the state
+    // to change it, use the setState() method, react stuff
+    // setState -> build (of the class that has it), rerenders stuff that only have to do with the state it changes
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
   @override // java decorator, good practice
   Widget build(BuildContext context) {
-    // MaterialApp does the base setup and it combines widgets into an app that can be rendered
-    // similar to python, we can use the position of args or just NAME them
-    // named and positional args
-    // home: <- core widget
+    // default list
+    var questions = [
+      'Why did she leave me?',
+      'How to fix a failing relationship?',
+      'How to escape existantial dred?'
+    ];
     return MaterialApp(
-      // scaffold is a util class with a basic color theme
       home: Scaffold(
         appBar: AppBar(
-          // create a bar widget and place a title in it
           title: Text('This be a tittle'),
         ),
-        body: Text('This be the body'),
+        // to put multiple widgets, need to use some layout widget first
+        // Column expects an array of widgets (Java structure esq)
+        // notation good practice <Widget>[]
+        body: Column(
+          children: [
+            // questions[0] or questions.elementAt(0)
+            Text(questions[_questionIndex]),
+            // RaisedButton in the prev version of flutter
+            // onPressed callback function child: Widget to render
+            // javascript stuff: passing a ref to the function, no the invocation
+            // answer > answer()
+
+            // again, anonymous vs named functions, shorthand and the long one
+            ElevatedButton(onPressed: _answer, child: Text('Answer 1')),
+            ElevatedButton(
+                // onPressed: () => print('Answered the second question'),
+                onPressed: _answer,
+                child: Text('Answer 2')),
+            ElevatedButton(
+                // onPressed: () {
+                //   print('Answered');
+                //   print('Answered twice');
+                // },
+                onPressed: _answer,
+                child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
