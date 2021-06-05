f71469ede3a9d24b8316d9985b501dcf02af03df
hyperinactive <hyperinactive.box@gmail.com>
2021-06-06 00:25:35 +0200

    basics_3

--- lib/answer.dart
+++ lib/answer.dart
@@ -4,17 +4,30 @@ import 'package:flutter/material.dart';
 // pointer to a function
 class Answer extends StatelessWidget {
   final Function selectHandler;
+  final String answerText;
 
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
     return Container(
         width: double.infinity,
         child: ElevatedButton(
-          child: Text('Answer 1'),
+          child: Text(answerText),
           onPressed: selectHandler,
           style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(Colors.pinkAccent)),
         ));
   }
 }
+
+// final vs const
+// runtime constant, compiled constant
+
+// for the final keyword, we can be in the dark as to what the variable will have, but not for const
+
+// variable cannot be changed
+// const smth = ['Hello'];
+// smth = []; --doesn't work
+// current value of the variable cannot be changed, but the variable can be assigned to a different one
+// var smth_2 = const ['Hello'];
+// smth_2 = []; --doesn work
--- lib/main.dart
+++ lib/main.dart
@@ -18,21 +18,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
+  // var questions = [
+  //   'Why did she leave me?',
+  //   'How to fix a failing relationship?',
+  //   'How to escape existantial dred?'
+  // ];
+
+  // using maps for demonstration purposes, doing it via class more traditional approach
+  // managing key-values pairs instead of props
+  // just a list of maps
+  static const questions = [
+    {
+      'questionText': 'Why did she leave me?',
+      'answers': ['No car', 'No income', 'Yes']
+    },
+    {
+      'questionText': 'How to fix a failing relationship?',
+      'answers': ['No shot', 'LMAO', 'Bandaid']
+    },
+    {
+      'questionText': 'How to escape existantial dred?',
+      'answers': ['Nah', 'Nope', 'No']
+    },
+  ];
 
   void _answer() {
     setState(() {
       _questionIndex++;
     });
-    print(_questionIndex);
   }
 
   @override // java decorator, good practice
   Widget build(BuildContext context) {
-    var questions = [
-      'Why did she leave me?',
-      'How to fix a failing relationship?',
-      'How to escape existantial dred?'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -41,17 +58,38 @@ class _MyAppState extends State<MyApp> {
         // to put multiple widgets, need to use some layout widget first
         // Column expects an array of widgets (Java structure esq)
         // notation good practice <Widget>[]
-        body: Column(
-          children: [
-            // Text(questions[_questionIndex]),
-            // now that Question widget is created, replace Text with it
-            Question(questions[_questionIndex]),
 
-            Answer(_answer),
-            Answer(_answer),
-            Answer(_answer),
-          ],
-        ),
+        // can use ternary op to display widgets conditionally
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  // Text(questions[_questionIndex]),
+                  // now that Question widget is created, replace Text with it
+                  Question(questions[_questionIndex]['questionText']),
+
+                  // can insert Widgets via map function like javascript
+                  // dart cannot know what type the map values are, so gotta use "as"
+                  // as List<String> -> promising dart that I know for sure I have a list of strings under "answers" keyword
+
+                  // getting the <Iterable> class and not List, so map doesn't work ->  .toList()
+
+                  // other problem, Collumn takes a list of Widgets, but we're giving it a list -> nesting lists basically
+                  // that won't do so we use the spread operator, make a copy this list's values so we end up with individual items
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answer, answer);
+                  }).toList()
+                  // Answer(_answer),
+                  // Answer(_answer),
+                  // Answer(_answer),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Thanks for answering!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
